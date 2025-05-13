3180664afa2d360aa0a0ae0623fc81f79ae374dd
fatimaaliyeva7 <eliyevaf777@gmail.com>
2025-05-13 14:09:06 +0000

    my task

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-"""Define a class Square with a private attribute size."""
+"""Define a class Square."""
+
+
 class Square:
-    """Represents a square with a private size attribute."""
-    def __init__(self, size):
-        """Initialize the Square instance with the given size.
-        Args:
-            size (int): The size of the square.
-        """
-        self.__size = size
+    """Represent a square."""
+    pass

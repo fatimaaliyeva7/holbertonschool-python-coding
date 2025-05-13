85acabf601a5d6ed7b9fa622dc6de29a4c72138b
fatimaaliyeva7 <eliyevaf777@gmail.com>
2025-05-13 05:39:03 +0000

    mytask

--- python-classes/0-main.py
+++ python-classes/0-main.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
 Square = __import__('0-square').Square
-my_square = Square(3)  # Burada size verilir
-print(type(my_square))
-print(my_square.__dict__)
+
+# Test 1
+my_square = Square(3)
+print(type(my_square))  # <class '__main__.Square'>
+print(my_square.__dict__)  # {'_Square__size': 3}
+
+# Test 2: Check for 'size' attribute, should raise error
 try:
     print(my_square.size)
 except Exception as e:
-    print(e)
+    print(e)  # 'Square' object has no attribute 'size'
+
+# Test 3: Check for '__size' attribute, should raise error
 try:
     print(my_square.__size)
 except Exception as e:
-    print(e)
+    print(e)  # 'Square' object has no attribute '__size'
+
--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 class Square:
     """A class that defines a square by its size (private)"""
+
     def __init__(self, size):
         """Initialize the square with a given size (no validation)"""
         self.__size = size
+
+

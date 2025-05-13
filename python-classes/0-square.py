b1cedba540ec2d899247ad0dfaca5dfce26acac4
fatimaaliyeva7 <eliyevaf777@gmail.com>
2025-05-13 05:35:15 +0000

    mytask

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 class Square:
     """A class that defines a square by its size (private)"""
-    
     def __init__(self, size):
         """Initialize the square with a given size (no validation)"""
         self.__size = size
-
